30eddbb4dcb245e63dc005491989881db4d3e795
sanim16 <sanim16@yahoo.com>
2022-12-13 01:06:24 +0100

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,14 @@ function App() {
           >
             Learn React
           </a>
+          <p>
+          ## multi-docker2
+          This repo updates multi-docker1 repo to use GitHub actions instead of TravisCI
+
+          ## Guide to Build a Multi-container App, test and Deploy Application in AWS EBS with AWS ElastiCache and AWS RDS using Github Actions
+
+          This project pushes a multi-container application to a GitHub Repo where it is tested then the images are pushed to Docker Hub. A docker-compose file is then used to deploy the application on AWS Elastic Beanstalk service making use of AWS RDS as a database and AWS ElastiCache as an In-Memory Cache.
+          </p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
